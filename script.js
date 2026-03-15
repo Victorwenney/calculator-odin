45099562cae7d02c06d249ff6b0333c4405d777d
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-15 11:15:59 -0300

    Create the numbers and the operator variables Create the operate function
    Created variables to store the numbers to be used and the operation and
    one variable to store the numerical representarion of the matematical
    operator itserlf.
    Created a function called 'operate'. This function recieves the operator
    and the two numbers and then calls one of the math operation function on
    the numbers it recieved, based on the operator.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numberA = 0;
+let numberB = 0;
+let operator = 0;
+
 function add(numberA, numberB) {
     return numberA + numberB;
 }
@@ -12,4 +16,14 @@ function multiply(numberA, numberB) {
 
 function divide(numberA, numberB) {
     return numberA / numberB;
+}
+
+function operate(numberA, numberB, operator) {
+    switch (operator) {
+        case 1: return add(numberA, numberB);
+        case 2: return subtract(numberA, numberB);
+        case 3: return multiply(numberA, numberB);
+        case 4: return divide(numberA, numberB);
+        default: return null;
+    }
 }
