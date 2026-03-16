3902ca3054f8d5be45578a32c473d2735072491e
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-16 09:43:36 -0300

    Add more style to buttons and display Add a class for the number buttons Add logic to update display on number button click
    Added a font-family, font-weight and modified font-size of the buttons.
    Added a class called 'numberBtn' for each of the number buttons.
    Added event listener do the number buttons to show the value on display
    like a calculator.

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-let numberA = 0;
-let numberB = 0;
+let currentNumber = '';
+let memoryNumber = '';
 let operator = 0;
 
-function add(numberA, numberB) {
-    return numberA + numberB;
+function add(memoryNumber, currentNumber) {
+    return +memoryNumber + +currentNumber;
 }
 
-function subtract(numberA, numberB) {
-    return numberA - numberB;
+function subtract(memoryNumber, currentNumber) {
+    return +memoryNumber - +currentNumber;
 }
 
-function multiply(numberA, numberB) {
-    return numberA * numberB;
+function multiply(memoryNumber, currentNumber) {
+    return +memoryNumber * +currentNumber;
 }
 
-function divide(numberA, numberB) {
-    return numberA / numberB;
+function divide(memoryNumber, currentNumber) {
+    return +memoryNumber / +currentNumber;
 }
 
-function operate(numberA, numberB, operator) {
+function operate(memoryNumber, currentNumber, operator) {
     switch (operator) {
-        case 1: return add(numberA, numberB);
-        case 2: return subtract(numberA, numberB);
-        case 3: return multiply(numberA, numberB);
-        case 4: return divide(numberA, numberB);
-        default: return null;
+        case 1: display.textContent = add(memoryNumber, currentNumber); break;
+        case 2: display.textContent = subtract(memoryNumber, currentNumber); break;
+        case 3: display.textContent = multiply(memoryNumber, currentNumber); break;
+        case 4: display.textContent = divide(memoryNumber, currentNumber); break;
+        default: break;
     }
 }
+
+const display = document.querySelector('#display');
+const numberBtn = Array.from(document.querySelectorAll('.numberBtn'));
+numberBtn.forEach(item => {
+    item.addEventListener('click', () => {
+        if(display.textContent != 0)
+            display.textContent += item.textContent;
+        else {
+            display.textContent = item.textContent;
+        }
+    });
+});
