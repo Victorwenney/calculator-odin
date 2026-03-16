da9e1167c56b6bcb84bbf0468332514c6b7b4bf6
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-16 16:02:54 -0300

    Reset the script.js file Create some basic function again
    Reseted the whole javascript file because I was tilted and was not going
    anywhere with the code.
    Created again the basic operation functions, the number button, the
    delete/backspace function and the clear function.

--- script.js
+++ script.js
@@ -20,50 +20,62 @@ function divide(memoryNumber, currentNumber) {
 
 function operate(memoryNumber, currentNumber, operator) {
     switch (operator) {
-        case 1: display.textContent = add(memoryNumber, currentNumber); break;
-        case 2: display.textContent = subtract(memoryNumber, currentNumber); break;
-        case 3: display.textContent = multiply(memoryNumber, currentNumber); break;
-        case 4: display.textContent = divide(memoryNumber, currentNumber); break;
+        case 1: display.textContent = add(memoryNumber, currentNumber);
+                memoryNumber = display.textContent; break;
+        case 2: display.textContent = subtract(memoryNumber, currentNumber);
+                memoryNumber = display.textContent; break;
+        case 3: display.textContent = multiply(memoryNumber, currentNumber);
+                memoryNumber = display.textContent; break;
+        case 4: display.textContent = divide(memoryNumber, currentNumber);
+                memoryNumber = display.textContent; break;
         default: break;
     }
 }
 
 const display = document.querySelector('#display');
+
 const numberBtn = Array.from(document.querySelectorAll('.numberBtn'));
 numberBtn.forEach(item => {
     item.addEventListener('click', () => {
-        if(display.textContent === '0' || currentNumber === ''){
-            display.textContent = item.textContent;
+        if(display.textContent != '0'){
             currentNumber += item.textContent;
         }else {
-            display.textContent += item.textContent;
-            currentNumber += item.textContent;
+            currentNumber = item.textContent;
         }
+        display.textContent = currentNumber;
     });
 });
 
 const operatorBtn = Array.from(document.querySelectorAll('.operatorBtn'));
 operatorBtn.forEach(item => {
     item.addEventListener('click', () => {
-        if(operator != 0){
-            operate(memoryNumber, currentNumber, operator);
-        }
-        memoryNumber = display.textContent;
-        currentNumber = '';
-        switch (item.getAttribute('id')) {
-            case 'plus': operator = 1; break;
-            case 'minus': operator = 2; break;
-            case 'multiply': operator = 3; break;
-            case 'divide': operator = 4; break;
-            default: break;
-        }      
+            
     });
 });
 
 const clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', () => {
-    display.textContent = '0';
-    operator = 0;
-    memoryNumber = '';
     currentNumber = '';
+    memoryNumber = '';
+    operator = 0;
+    display.textContent = 0;
+});
+
+const equalsBtn = document.querySelector('#equalsBtn');
+equalsBtn.addEventListener('click', () => {
+    
+})
+
+const delBtn = document.querySelector('#delBtn');
+delBtn.addEventListener('click', () => {
+    if(display.textContent.slice(0, display.textContent.length - 1) === ''){
+        display.textContent = 0;
+    }else {
+        display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    }
+});
+
+const commaBtn = document.querySelector('#commaBtn');
+commaBtn.addEventListener('click', () => {
+
 });
