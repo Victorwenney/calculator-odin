61be8ccf7563bfb19abab875c31da65fff686c7a
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-16 17:16:21 -0300

    Improve the logic of the equals button Improve the logic of the del/backspace button
    Improves the logic of both buttons to that is behaves closely to a real
    calculator.

--- script.js
+++ script.js
@@ -20,14 +20,10 @@ function divide(memoryNumber, currentNumber) {
 
 function operate(memoryNumber, currentNumber, operator) {
     switch (operator) {
-        case 1: display.textContent = add(memoryNumber, currentNumber);
-                memoryNumber = display.textContent; break;
-        case 2: display.textContent = subtract(memoryNumber, currentNumber);
-                memoryNumber = display.textContent; break;
-        case 3: display.textContent = multiply(memoryNumber, currentNumber);
-                memoryNumber = display.textContent; break;
-        case 4: display.textContent = divide(memoryNumber, currentNumber);
-                memoryNumber = display.textContent; break;
+        case 1: display.textContent = add(memoryNumber, currentNumber); break;
+        case 2: display.textContent = subtract(memoryNumber, currentNumber); break;
+        case 3: display.textContent = multiply(memoryNumber, currentNumber); break;
+        case 4: display.textContent = divide(memoryNumber, currentNumber); break;
         default: break;
     }
 }
@@ -37,22 +33,21 @@ const display = document.querySelector('#display');
 const numberBtn = Array.from(document.querySelectorAll('.numberBtn'));
 numberBtn.forEach(btn => {
     btn.addEventListener('click', () => {
-        if(display.textContent != '0'){
+        if(display.textContent != '0' && !operatorToggle){
             currentNumber += btn.textContent;
         }else {
             currentNumber = btn.textContent;
         }
         display.textContent = currentNumber;
+        operatorToggle = false;
     });
 });
 
 const operatorBtn = Array.from(document.querySelectorAll('.operatorBtn'));
+let operatorToggle = false;
 operatorBtn.forEach(btn => {
     btn.addEventListener('click', () => {
-            //Passing display.textContent instead of currentNumber in case an operator is pressed numerous times and the
-            //currentNumber variable is already '', this way we prevent memoryNumber to also turn ''.
-            memoryNumber = display.textContent;
-            currentNumber = '';
+            memoryNumber = display.textContent;   
             switch (btn.id) {
                 case 'plus': operator = 1; break;
                 case 'minus': operator = 2; break;
@@ -60,6 +55,7 @@ operatorBtn.forEach(btn => {
                 case 'divide': operator = 4; break;
                 default: break; 
             }
+            operatorToggle = true;
     });
 });
 
@@ -69,12 +65,15 @@ clearBtn.addEventListener('click', () => {
     memoryNumber = '';
     operator = 0;
     display.textContent = 0;
+    operatorToggle = false;
 });
 
 const equalsBtn = document.querySelector('#equalsBtn');
 equalsBtn.addEventListener('click', () => {
     operate(memoryNumber, currentNumber, operator);
-})
+    memoryNumber = display.textContent;
+    operatorToggle = false;
+});
 
 const delBtn = document.querySelector('#delBtn');
 delBtn.addEventListener('click', () => {
@@ -83,6 +82,8 @@ delBtn.addEventListener('click', () => {
     }else {
         display.textContent = display.textContent.slice(0, display.textContent.length - 1);
     }
+    currentNumber = display.textContent;
+    operatorToggle = false;
 });
 
 const commaBtn = document.querySelector('#commaBtn');
@@ -91,4 +92,5 @@ commaBtn.addEventListener('click', () => {
         display.textContent += commaBtn.textContent;
         currentNumber = display.textContent;
     }
+    operatorToggle = false;
 });
